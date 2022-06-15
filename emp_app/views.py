af058e7840f05c0cebe4a6dcdab0b0899bf791c5
MorgDzh <dzhanaliev.aktan@gmail.com>
2022-06-15 23:00:56 +0600

    added add employees webpage

--- emp_app/views.py
+++ emp_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .models import Employee, Role, Department
+from datetime import datetime
 
 
 # Create your views here.
@@ -17,7 +18,22 @@ def all_emp(request):
 
 
 def add_emp(request):
-    return render(request, 'add_emp.html')
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        salary = int(request.POST['salary'])
+        bonus = int(request.POST['bonus'])
+        phone = int(request.POST['phone'])
+        dept = int(request.POST['dept'])
+        role = int(request.POST['role'])
+        new_emp = Employee(first_name=first_name, last_name=last_name, salary=salary, bonus=bonus, phone=phone,
+                           dept_id=dept, role_id=role, hire_date=datetime.now())
+        new_emp.save()
+        return HttpResponse('Employee added Successfully')
+    elif request.method == 'GET':
+        return render(request, 'add_emp.html')
+    else:
+        return HttpResponse("An Exception Occured! Employee Has Not Been Added")
 
 
 def remove_emp(request):
